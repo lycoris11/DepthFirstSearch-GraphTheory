9ab9e054ca06910339d88b25f235e1dd7661b53e
Mark Pinto <varun.pinto98@gmail.com>
2017-03-18 00:48:56 -0500

    Implemented depthFirstSearch Algorithm

--- src/lab1.java
+++ src/lab1.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -19,8 +21,18 @@ public final class lab1 {
     /**
      * Runs my implementation of the DepthFirstSearch Algorithm on a 2D array.
      */
-    private static void depthFirstSearch() {
+    private static void depthFirstSearch(int[] v, int[][] a, int i) {
 
+        v[i] = 1;
+        System.out.print(" " + Math.addExact(i, 1));
+
+        for (int j = 0; j < a[i].length; j++) {
+            if (a[i][j] == 1) {
+                if (v[j] == 0) {
+                    depthFirstSearch(v, a, j);
+                }
+            }
+        }
     }
 
     /**
@@ -31,10 +43,20 @@ public final class lab1 {
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        File file = new File("data1.txt");
+
+        try {
+            in = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         //do {
         int n = in.nextInt();
         int[][] arr = new int[n][n];
+        int[] vList = new int[n];
+        in.nextLine();
 
         for (int i = 0; i < n; i++) {
             String rowEntry = in.nextLine();
@@ -49,6 +71,22 @@ public final class lab1 {
                 }
             }
         }
+
+        int numComponents = 1;
+        System.out.print("Component:");
+        depthFirstSearch(vList, arr, 0);
+
+        for (int k = 0; k < vList.length; k++) {
+            if (vList[k] == 0) {
+                numComponents++;
+                System.out.println();
+                System.out.print("Component:");
+                depthFirstSearch(vList, arr, k);
+            }
+        }
+
+        System.out.println();
+        System.out.print("Total number of Components: " + numComponents);
 
         //} while (in.hasNextLine());
 
